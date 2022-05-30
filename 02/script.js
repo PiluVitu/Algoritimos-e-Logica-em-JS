27c0bc4d3a3f74098b68ed9c38de77661dcc1a4a
Paulo Victor <Paulo.tspi@gmail.com>
2022-05-30 17:32:30 -0300

    02 complete

---
+++ 02/script.js
@@ -0,0 +1,26 @@
+/*
+Solicite 2 números, faça a soma deles e apresente o resultado final ao usuário 
+*/
+alert('Vamos fazer uma soma :')
+
+let numberOne = prompt('Digite o primeiro número')
+let numberTwo = prompt('Digite o segundo numero')
+let sumResult = Number(numberOne) + Number(numberTwo)
+
+alert('O resultado da soma foi: ' + sumResult)
+
+alert('Vamos fazer uma divisão :')
+
+let numberThree = prompt('Digite o primeiro número')
+let numberFour = prompt('Digite o segundo numero')
+let divResult = Number(numberThree) / Number(numberFour)
+
+alert('O resultado da divisão foi: ' + divResult)
+
+alert('Vamos fazer uma multiplicação :')
+
+let numFive = prompt('Digite o 1 numero :')
+let numSix = prompt('Digite o 2 numero :')
+let multiResult = Number(numFive) * Number(numSix)
+
+alert('O resultado da multiplicação é: ' + multiResult)
--- script.js
+++
@@ -1,4 +0,0 @@
-/*
-Solicite 2 números, faça a soma deles e apresente o resultado final ao usuário 
-*/
-
